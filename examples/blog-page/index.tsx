f90d4aab0376004055f543cba59758773e47ddaf
rakeeb-hossain <rakeeb.hossain1@gmail.com>
2022-04-26 22:45:50 -0400

    skeleton

---
+++ examples/blog-page/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import {ReactDOM} from "react";
+import {createRoot} from "react-dom/client";
+
+const App = () => {
+    return <div/>
+}
+
+const root = createRoot(document.getElementById('root')!);
+root.render(<App/>);
---
+++ packages/ghost-loading/src/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export interface GhostProps {
+
+}
+
+export const Ghost = (props: GhostProps) => {
+    
+}
